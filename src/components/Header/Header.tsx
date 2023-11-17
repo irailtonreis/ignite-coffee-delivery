6034fdf7b7f50f5ed0e186409cf18b08d4121d58
irailtonreis <irailton.oliveira91@gmail.com>
2023-11-17 19:16:08 -0300

    feat: Context - Exibe quantidade no header cart

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { HeaderContainer, Address, StyledNavLink } from "./styles";
 import { NavLink } from "react-router-dom";
 import logoCoffeeDelivery from "../../assets/logo-coffe-delivery.svg";
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { OrderContext } from "../../Contexts/OrderContext";
+
 
 const Header: React.FC = () => {
+  const { order } = useContext(OrderContext)
   return (
     <HeaderContainer>
       <NavLink to="/">
@@ -17,7 +20,7 @@ const Header: React.FC = () => {
           <MapPin size={22} color="#8047F8" />
           <p>Porto Alegre, RS</p>
         </Address>
-        <StyledNavLink to="/checkout" quantity={3}>
+        <StyledNavLink to="/checkout" quantity={order.length}>
           <ShoppingCart  color="#C47F17" size={22}/>
         </StyledNavLink>
       </nav>
